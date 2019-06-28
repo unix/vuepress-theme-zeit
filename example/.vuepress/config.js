b12ac7fc847ab00de2abfbf9b5f5da3142b9974e
WittBulter <lambdas.dev@gmail.com>
2019-06-28 15:25:36 +0800

    feat: add examples

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -1,36 +1,72 @@
 module.exports = {
-  title: 'zeit-ui',
+  title: 'func - modern command-line framework',
   description: '',
   head: [
     ['link', { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' }],
   ],
-  theme: require.resolve('../..'),
-  themeConfig: {
-    lastUpdated: 'Last Updated',
-    nav: [
-      { text: 'Guide', link: '/' },
-      { text: 'Github', link: 'https://github.com/wittbulter' },
-      { text: 'options', items: [{
-        text: 'one', link: '/w',
-        }]}
-    ],
-    sidebar: {
-      '/': [{
-        title: 'Guide',
-        children: [
-          '/',
-          'about',
-          'install',
-          {
-            title: 'deployment',
-            children: [
-              '/deployment/basic.md',
-              '/deployment/lifecycle.md',
-            ],
-          },
-          'theme',
-        ]
-      }]
-    }
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'func - modern command-line framework',
+      description: 'func - modern command-line framework',
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'func - 现代化命令行框架',
+      description: 'func - 现代化命令行框架',
+    },
   },
+  theme: require.resolve('../../'),
+  themeConfig: {
+    repo: 'wittbulter/func',
+    docsDir: 'docs',
+    editLinks: true,
+    lastUpdated: 'Last Updated',
+    highlightCode: true,
+    locales: {
+      '/': {
+        nav: [
+          { text: 'Guide', link: '/guide' },
+          { text: 'Github', link: 'https://github.com/wittbulter/func' },
+        ],
+        sidebar: {
+          '/': [
+            '/',
+            '/guide',
+            './command',
+            './params',
+            './publish',
+            './apis',
+          ],
+        },
+      },
+      '/zh/': {
+        docsDir: 'docs/zh',
+        selectText: '选择语言',
+        label: '简体中文',
+        nav: [
+          { text: '快速开始', link: '/zh/guide' },
+          { text: '项目', link: 'https://github.com/wittbulter/func' },
+        ],
+        sidebar: {
+          '/zh/': [
+            '/zh/',
+            '/zh/guide',
+            '/zh/command',
+            '/zh/params',
+            '/zh/publish',
+            '/zh/apis',
+          ],
+        },
+      },
+    },
+  },
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-110371817-9',
+      },
+    ],
+  ],
 }
